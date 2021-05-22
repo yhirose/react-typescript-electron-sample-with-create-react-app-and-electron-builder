140458a21f2353a8311310bedfc0dceb61e471f2
yhirose <yuji.hirose.bug@gmail.com>
2021-05-22 13:54:52 -0400

    Added preload.ts

--- electron/main.ts
+++ electron/main.ts
@@ -3,14 +3,13 @@ import * as path from 'path';
 import * as isDev from 'electron-is-dev';
 import installExtension, { REACT_DEVELOPER_TOOLS } from "electron-devtools-installer";
 
-let win: BrowserWindow | null = null;
-
 function createWindow() {
-  win = new BrowserWindow({
+  const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      // contextIsolation: false,
+      preload: path.join(__dirname, 'preload.js')
     }
   })
 
@@ -20,8 +19,6 @@ function createWindow() {
     // 'build/index.html'
     win.loadURL(`file://${__dirname}/../index.html`);
   }
-
-  win.on('closed', () => win = null);
 
   // Hot Reloading
   if (isDev) {
@@ -38,26 +35,28 @@ function createWindow() {
     });
   }
 
-  // DevTools
-  installExtension(REACT_DEVELOPER_TOOLS)
-    .then((name) => console.log(`Added Extension:  ${name}`))
-    .catch((err) => console.log('An error occurred: ', err));
-
   if (isDev) {
     win.webContents.openDevTools();
   }
 }
 
-app.on('ready', createWindow);
+app.whenReady().then(() => {
+  // DevTools
+  installExtension(REACT_DEVELOPER_TOOLS)
+    .then((name) => console.log(`Added Extension:  ${name}`))
+    .catch((err) => console.log('An error occurred: ', err));
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+  createWindow();
 
-app.on('activate', () => {
-  if (win === null) {
-    createWindow();
-  }
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
 });
